b438e2eefb81a590e63542f2d793003102c66d10
israxx97 <isragarcia97@gmail.com>
2019-03-07 14:30:56 +0100

    + v0.2 + Subido el index.php y parte de la práctica U00_A00.html.

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="./../webroot/images/favicon2.png">
+        <title>DAW - Israel García Cabañeros</title>
+        <!-- Bootstrap core CSS -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!-- Custom styles for this template -->
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+        <link href="blog.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <header class="blog-header py-3">
+                <div class="row flex-nowrap justify-content-between align-items-center">
+                    <div class="col-4 pt-1">
+                        <!-- <a class="text-muted" href="#">DAW - ISRAEL GARCÍA CABAÑEROS</a> -->
+                    </div>
+                    <div class="col-4 text-center">
+                        <!-- <a class="blog-header-logo text-dark" href="#">
+                            <img style="width: 30px; height: 30px;" alt="imagen" src="./../webroot/images/favicon2.png">
+                        </a> -->
+                    </div>
+                    <div class="col-4 d-flex justify-content-end align-items-center">
+                        <!-- <a class="text-muted" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+                        </a> -->
+                        <!-- <a class="btn btn-sm btn-outline-secondary" href="../index.php">Sign up</a> -->
+                    </div>
+                </div>
+            </header>
+
+            <!-- <div class="nav-scroller py-1 mb-2">
+                <nav class="nav d-flex justify-content-between">
+                    <a class="p-2 text-muted" href="#">World</a>
+                    <a class="p-2 text-muted" href="#">U.S.</a>
+                    <a class="p-2 text-muted" href="#">Technology</a>
+                    <a class="p-2 text-muted" href="#">Design</a>
+                    <a class="p-2 text-muted" href="#">Culture</a>
+                    <a class="p-2 text-muted" href="#">Business</a>
+                    <a class="p-2 text-muted" href="#">Politics</a>
+                    <a class="p-2 text-muted" href="#">Opinion</a>
+                    <a class="p-2 text-muted" href="#">Science</a>
+                    <a class="p-2 text-muted" href="#">Health</a>
+                    <a class="p-2 text-muted" href="#">Style</a>
+                    <a class="p-2 text-muted" href="#">Travel</a>
+                </nav>
+            </div> -->
+
+            <div style="background: url(webroot/images/o-SOCCER-BALL-facebook.jpg)" class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+                <div class="col-md-6 px-0">
+                    <h1 class="display-4 font-italic">DAW - Israel García Cabañeros</h1>
+                    <!-- <p class="lead my-3">Este blog ha sido construido para el proyecto de empresa del CEO de Forobet, Israel García Cabañeros.</p> -->
+                    <!-- <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p> -->
+                </div>
+            </div>
+
+            <div class="row mb-2">
+                <div class="col-md-6">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                        <div class="card-body d-flex flex-column align-items-start">
+                            <h3 class="d-inline-block mb-2 text-warning">
+                                INDEX
+                                <!-- <img style="width: 50px; height: 50px;" class="card-img-right flex-auto d-none d-md-block" src="./../webroot/images/vkdhMjOq_400x400.jpg" alt="Card image cap"> -->
+                            </h3>
+                            <!-- <p class="card-text mb-auto">Chollometro es una página que sube productos rebajados y ayuda a la página mediante promociones.</p> -->
+                            <a href="../index.html">Accede al Index</a>
+                        </div> 
+                    </div>
+                </div>
+                <!-- <div class="col-md-6">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                        <div class="card-body d-flex flex-column align-items-start">
+                            <h3 class="d-inline-block mb-2 text-success">
+                                Bet365
+                                <img style="width: 50px; height: 50px;" class="card-img-right flex-auto d-none d-md-block" src="./../webroot/images/5a33af9b4a762_1513336731.png" alt="Card image cap">
+                            </h3>
+                            <p class="card-text mb-auto">Bet365 es una página de apuestas que nos ayuda a crecer económicamente afiliandonos a ellos y ganando parte de los beneficios con los usuarios registrados en su página desde la nuestra.</p>
+                            <a href="https://www.bet365.es/#/HO/">Accede a su página</a>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+
+        <main role="main" class="container">
+            <div class="row">
+                <div class="col-md-8 blog-main">
+                    <h3 class="pb-3 mb-4 font-italic border-bottom">
+                        Índice de prácticas.
+                    </h3>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="html/U00_A00.html">U00 - A00</a></h2>
+                        <p class="blog-post-meta">07/03/2019, 10:49</p>
+                        <p>En esta práctica inicial vamos a repasar los conceptos básicos TCP/IP, además de las arquitecturas y tecnologías web.</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U01_A01.html">U01 - A01</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                        <p>En esta práctica vamos a documentar las características de la máquina anfitriona del centro, indicando entre otras cosas el Sistema Operativo utilizado, Memoria RAM, usuarios, etc...</p>
+                        <p>Además, encontraremos el software usado en este ciclo para el desarrollo (IDEs, navegadores, etc...)</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U01_A02.html">U01 - A02</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U02_A01.html">U02 - A01</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U02_A02.html">U02 - A02</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U02_A03.html">U02 - A03</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U02_A04.html">U02 - A04</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U04_A01.html">U04 - A01</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                    </div>
+
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><a href="U04_A02.html">U04 - A02</a></h2>
+                        <p class="blog-post-meta">//2019, :</p>
+                    </div>
+                </div>   
+
+                <aside class="col-md-4 blog-sidebar">
+                    <div class="p-3 mb-3 bg-light rounded">
+                        <h4 class="font-italic">Sobre otras asignaturas...</h4>
+                        <!-- <p class="mb-0">Forobet sigue las normativas que regulan al comercio electrónico.</p> -->
+                        <ul class="mb-0">
+                            <li><a href="../ProyectoDWES/index.php">DWES</a></li>
+                            <li><a href="../ProyectoDWEC/index.php" onclick="return false">DWEC</a></li>
+                            <li><a href="../ProyectoDIW/index.php" onclick="return false">DIW</a></li>
+                            <li><a href="../ProyectoEIE/index.php" onclick="return false">EIE</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="p-3">
+                        <h4 class="font-italic">Tecnologías usadas...</h4>
+                        <ol class="list-unstyled mb-0">
+                            <li><a href="https://www.w3schools.com/html/">HTML</a></li>
+                            <li><a href="https://www.w3schools.com/css/">CSS</a></li>
+                            <li><a href="http://www.php.net/">PHP</a></li>
+                            <li><a href="https://getbootstrap.com/">Bootstrap</a></li>
+                            <li><a href="https://jquery.com/">JQuery</a></li>
+                            <!-- <li><a href="https://www.hostinger.es/">Hostinger</a></li> -->
+                        </ol>
+                    </div>
+
+                    <div class="p-3">
+                        <h4 class="font-italic">Repositorio en...</h4>
+                        <ol class="list-unstyled">
+                            <!-- <li><a href="https://www.instagram.com/forobet.oficial/">Instagram</a></li> -->
+                            <li><a href="https://github.com/israxx97/ProyectoDAW">GitHub</a></li>
+                        </ol>
+                    </div>
+                </aside><!-- /.blog-sidebar -->
+            </div><!-- /.row -->
+        </main><!-- /.container -->
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+        <script src="../../assets/js/vendor/popper.min.js"></script>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="../../assets/js/vendor/holder.min.js"></script>
+        <script>
+                                Holder.addTheme('thumb', {
+                                    bg: '#55595c',
+                                    fg: '#eceeef',
+                                    text: 'Thumbnail'
+                                });
+        </script>
+    </body>
+</html>
